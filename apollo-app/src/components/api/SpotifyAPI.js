fc8c6f4307b461188d5ee0f85ad788200a87a9dc
Y07-assine <yassinemerzouki.ym@gmail.com>
2021-04-16 01:21:27 +0000

    modifie spotify api component

--- apollo-app/src/components/api/SpotifyAPI.js
+++ apollo-app/src/components/api/SpotifyAPI.js
@@ -7,6 +7,7 @@ class SpotifyAPI extends Component{
     state ={
         token:'',
         error:null,
+        playlistimage:'',
         data: []
       };
       componentDidMount(){
@@ -23,7 +24,7 @@ class SpotifyAPI extends Component{
           .then(res =>{
             console.log(res.data.access_token);
             this.setState({token:res.data.access_token});
-            axios('https://api.spotify.com/v1/playlists/37i9dQZF1DWYtEjm4ihp5w/tracks?offset=0&limit=5&market=US',{
+            axios('https://api.spotify.com/v1/playlists/37i9dQZF1DWYtEjm4ihp5w/tracks?offset=0&limit=7&market=US',{
               method:'GET',
               headers:{
                 'Content-Type':'application/json',
@@ -34,6 +35,17 @@ class SpotifyAPI extends Component{
               console.log(playlistresponse.data)
               this.setState({data:playlistresponse.data.items})
           });
+          axios('https://api.spotify.com/v1/playlists/37i9dQZF1DWYtEjm4ihp5w/images',{
+              method:'GET',
+              headers:{
+                'Content-Type':'application/json',
+                  'Authorization':'Bearer ' + res.data.access_token
+                }
+          })
+          .then(imageresponse =>{
+              console.log(imageresponse.data)
+              this.setState({playlistimage:imageresponse.data[0]})
+          });
           })
           .catch(err => {
             this.setState({error:err});
@@ -43,34 +55,41 @@ class SpotifyAPI extends Component{
           
       }
     render(){
-        const {data, error ,loading} = this.state;
+        const {data,playlistimage, error ,loading} = this.state;
         return(
             <section className="py-5 news">
-               <div className="classement__item row">
-               <table class="table">
-                <thead>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">TITLE</th>
-                    <th scope="col">ARTISTE</th>
-                    <th scope="col">ALBUM</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {data.map((item)=>(
-                    <tr>
-                    <td><img src={item.track.album.images[0].url} className="image_item" /></td>
-                    <td> {item.track.name}</td>
-                    <td>{item.track.artists.map((artist)=>(
-                        <span>{artist.name}</span>
-                    ))}
-                    </td>
-                    <td>{item.track.album.name}</td>
-                    </tr>
-                ))}
-                
-                </tbody>
-                </table>
+               <div className="classement__item row card ">
+               <div className='card-inner'>
+                <div className='card-front'>
+                  <img src={playlistimage.url} />
+                  </div>
+                  <div className='card-back'>
+                    <table class="table">
+                      <thead>
+                          <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">TITLE</th>
+                          <th scope="col">ARTISTE</th>
+                          <th scope="col">ALBUM</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                      {data.map((item)=>(
+                          <tr>
+                          <td><img src={item.track.album.images[0].url} className="image_item" /></td>
+                          <td> {item.track.name}</td>
+                          <td>{item.track.artists.map((artist)=>(
+                              <span>{artist.name}</span>
+                          ))}
+                          </td>
+                          <td>{item.track.album.name}</td>
+                          </tr>
+                      ))}
+                      
+                      </tbody>
+                      </table>
+                  </div>
+                </div>
                </div>
             </section>
         )
