18e4a19cda6bdbbb1690e59892a1a32e9fa4e55a
Y07-assine <yassinemerzouki.ym@gmail.com>
2021-04-24 18:05:49 +0000

    modifie footer

--- apollo-app/src/components/Footer.js
+++ apollo-app/src/components/Footer.js
@@ -1,5 +1,8 @@
 import React,{ Component} from 'react';
-
+import logo from '../img/logo.png';
+import fbIcon from '../icon/icons8-facebook-24.png';
+import instaIcon from '../icon/icons8-instagram-26.png';
+import twitterIcon from '../icon/icons8-twitter-24.png';
 
 
 
@@ -13,19 +16,23 @@ render(){
         <div class="contianer">
             <div class="footer__top">
                 <div class="footer-top__box">
-                    <h3>A PROPOS DE NOUS</h3>
-                    <h5>Apollo for Music is Is Dedicated To Bringing The News Of Moroccan Rap  To A Wide Audience Without Bias Or A benefit Agenda </h5>
-                    <span>Contact Us: apollomusic@gmail.com</span>
+                    <img src={logo} className="logo" />
+                    <div className="socialMedia">
+                        <span>
+                        <a href="#"><img src={fbIcon}/></a>
+                        <a href="#"><img src={instaIcon}/></a>
+                        <a href="#"><img src={twitterIcon}/></a> 
+                        </span> 
+                    </div>
+                    
                 </div>
                 <div class="footer-top__box">
-                    <h3>FOLLOW US</h3>
-                    <a href="#">Instagram</a>
-                    <a href="#">Facebook</a>
-                    <a href="#">Twitter</a>
+                    <h3>About Apollo Music</h3>
+                    <p>Apollo for Music is Is Dedicated To Bringing The News Of Moroccan Rap To A Wide Audience Without Bias Or A benefit Agenda.</p>
+                    <span>Contact Us: apollomusic@gmail.com</span>
                 </div>
-            <div class="text-center text-white">Copyrights 2021. By EL MERZOUKI Yassine</div>
+            
         </div>
-    
         </div>
     </footer>
       
